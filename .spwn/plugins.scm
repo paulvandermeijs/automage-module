03033ae2abacf0cfd3abab3519b878aa6ddd6cbb
Paul van der Meijs <vandermeijs@redkiwi.nl>
2025-03-19 10:12:06 +0100

    feat: Add cwd function to locate project directory in plugins.scm

--- .spwn/plugins.scm
+++ .spwn/plugins.scm
@@ -20,3 +20,22 @@
   (cond
     [(string-contains? module " ") "Module name can't contain spaces"]
     [else #t]))
+
+; Find Magento root
+(define (cwd cwd)
+  (define (search path)
+    (if (string=? path "")
+      #f
+      (let ((composer-path (string-append path "/composer.json")))
+        (if (is-file? composer-path)
+          (let* ((file-str (read-port-to-string (open-input-file composer-path)))
+                 (data (string->jsexpr file-str)))
+            (if (and (hash-contains? data 'type)
+                 (string=? (hash-ref data 'type) "project"))
+              path
+              (search (parent-name path))))
+          (search (parent-name path))))))
+  (let ((found (search cwd)))
+    (if found
+      (string-append found "/app/code")
+      cwd)))
